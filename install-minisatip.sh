a068c7a4105aa8ee2295598a43fa0bfc2b5fe229
dnmnhat <68333160+dnmnhat@users.noreply.github.com>
2023-05-30 00:02:38 +0700

    Update install-minisatip.sh

--- install-minisatip.sh
+++ install-minisatip.sh
@@ -5,7 +5,7 @@ git clone https://github.com/dnmnhat/minisatip.git
 # move to new folder
 cd minisatip/
 # configure minisatip
-./configure make
+./configure --host=mips-openwrt-linux-musl --enable-static
 # build minisatip
 make
 # start minisat with parameter
